6a8797ba67307152b0d7e88ff6e80e1fffbb7e20
Karen <kjgamis@live.ca>
2018-06-21 16:03:50 -0400

    only display weather components if props are defined

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,10 +5,10 @@ class Weather extends Component {
     return (
       <div>
         { this.props.city && this.props.country && <p>Location:    { this.props.city } { this.props.country }</p> }
-        { this.props.city && this.props.country && <p>Temperature: { this.props.temperature }</p> }
-        { this.props.city && this.props.country && <p>Conditions:  { this.props.description }</p> }
-        { this.props.city && this.props.country && <p>Humidity:    { this.props.humidity }</p> }
-        { this.props.city && this.props.country && <p>{ this.props.error }</p> }
+        { this.props.temperature && <p>Temperature: { this.props.temperature }</p> }
+        { this.props.description && <p>Conditions:  { this.props.description }</p> }
+        { this.props.humidity && <p>Humidity:    { this.props.humidity }</p> }
+        { this.props.error && <p>{ this.props.error }</p> }
       </div>
     )
   }
