9c23b429876039bcbfb58d463033fa14bd40e890
Karen <kjgamis@live.ca>
2018-06-21 15:39:18 -0400

    define initializer

--- src/App.js
+++ src/App.js
@@ -8,10 +8,36 @@ import Weather from './components/Weather'
 const API_KEY = '9b1f3a63c822bb3747599ee339af431e'
 
 class App extends Component {
-  getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=toronto&appid=${API_KEY}&units=metric`)
+  state = {
+    temperature: undefined,
+    city:        undefined,
+    country:     undefined,
+    humidity:    undefined,
+    description: undefined,
+    error:       undefined
+
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault()
+
+    // values from input
+    const city    = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
     const data     = await api_call.json()
     console.log(data)
+
+    this.setSate({
+      temperature: data.main.temp,
+      city:        data.name,
+      country:     data.sys.country,
+      humidity:    data.main.humidity,
+      description: data.weather[0].description,
+      error:       ''
+    })
+
   }
 
   render() {
@@ -19,7 +45,7 @@ class App extends Component {
       <div>
         <h1>This is the container</h1>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather}/>
         <Weather />
       </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 class Form extends Component {
   render() {
     return (
-      <div>
-        <p>Form Component</p>
-      </div>
+      <form onSubmit={this.props.getWeather}>
+        <input type="text" placeholder="City..."    name="city"/>
+        <input type="text" placeholder="Country..." name="country"/>
+        <button>Get Weather</button>
+      </form>
     )
   }
 }
