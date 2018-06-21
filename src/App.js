1abbea9d7f202db6621fdde02bf84cc0e8cb5857
Karen <kjgamis@live.ca>
2018-06-21 14:54:10 -0400

    add api cal with await

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Titles from './components/Titles'
 import Form from './components/Form'
 import Weather from './components/Weather'
 
+const API_KEY = '9b1f3a63c822bb3747599ee339af431e'
+
 class App extends Component {
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=toronto&appid=${API_KEY}&units=metric`)
+    const data     = await api_call.json()
+    console.log(data)
+  }
+
   render() {
     return (
       <div>
