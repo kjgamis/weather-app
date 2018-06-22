8ecda24c0025a309129e838c987fdae93d8c9c01
Karen <kjgamis@live.ca>
2018-06-21 20:56:17 -0400

    able to move current weather to new component

--- src/App.js
+++ src/App.js
@@ -1,100 +1,23 @@
 import React, { Component } from 'react';
 
-import Titles from './components/Titles'
-import Form from './components/Form'
-import Weather from './components/Weather'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
-const API_KEY = '9b1f3a63c822bb3747599ee339af431e'
+import Titles  from './components/Titles'
+import Form    from './components/Form'
+import Weather from './components/Weather'
+import Current from './components/Current'
 
 class App extends Component {
-  state = {
-    city:        undefined,
-    country:     undefined,
-    temperature: undefined,
-    description: undefined,
-    humidity:    undefined,
-    error:       undefined
 
-  }
-
-  getWeather = async (e) => {
-    e.preventDefault()
-
-    // values from input
-    const city    = e.target.elements.city.value;
-    const country = e.target.elements.country.value;
-
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
-    const data     = await api_call.json()
-    console.log(data)
-
-    // avoids breaking the app if button is clicked with blank values in the form
-    if (city && country) {
-      this.setState({
-        city:        data.name,
-        country:     data.sys.country,
-        temperature: data.main.temp,
-        description: data.weather[0].description,
-        humidity:    data.main.humidity,
-        error:       ''
-      });
-    } else {
-      this.setState({
-        city:        undefined,
-        country:     undefined,
-        temperature: undefined,
-        description: undefined,
-        humidity:    undefined,
-        error:       'Please enter a valid city and country'
-      })
-    }
-
-  }
-
-  clearForm = e => {
-    e.preventDefault();
-    
-    this.setState({
-      city:        undefined,
-      country:     undefined,
-      temperature: undefined,
-      description: undefined,
-      humidity:    undefined,
-      error:       undefined
-    })
-  }
 
   render() {
     return (
       <div>
-        <div className="wrapper">
-          <div className="main">
-              <div className="row">
-
-                <div className="col-md-5 title-container">
-                  <Titles />
-                </div>
-
-                <div className="col-md-7 form-container">
-                  <Form
-                    getWeather={this.getWeather}
-                  />
-                  <Weather
-                    city       ={this.state.city}
-                    country    ={this.state.country}
-                    temperature={this.state.temperature}
-                    description={this.state.description}
-                    humidity   ={this.state.humidity}
-                    error      ={this.state.error}
-                  />
-                </div>
-
-              </div>
-            </div>
-        </div>
-
-
-
+        <Current />
       </div>
     );
   }
---
+++ src/components/Current.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+
+import Titles  from './Titles'
+import Form    from './Form'
+import Weather from './Weather'
+
+class Current extends Component {
+  state = {
+    city:        undefined,
+    country:     undefined,
+    temperature: undefined,
+    description: undefined,
+    humidity:    undefined,
+    error:       undefined
+
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault()
+
+    // values from input
+    const city    = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const API_KEY = '9b1f3a63c822bb3747599ee339af431e'
+
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
+    const data     = await api_call.json()
+    console.log(data)
+
+    // avoids breaking the app if button is clicked with blank values in the form
+    if (city && country) {
+      this.setState({
+        city:        data.name,
+        country:     data.sys.country,
+        temperature: data.main.temp,
+        description: data.weather[0].description,
+        humidity:    data.main.humidity,
+        error:       ''
+      });
+    } else {
+      this.setState({
+        city:        undefined,
+        country:     undefined,
+        temperature: undefined,
+        description: undefined,
+        humidity:    undefined,
+        error:       'Please enter a valid city and country'
+      })
+    }
+
+  }
+
+  clearForm = e => {
+    e.preventDefault();
+
+    this.setState({
+      city:        undefined,
+      country:     undefined,
+      temperature: undefined,
+      description: undefined,
+      humidity:    undefined,
+      error:       undefined
+    })
+  }
+
+  render() {
+    return(
+        <div className="wrapper">
+          <div className="main">
+              <div className="row">
+
+                <div className="col-md-5 title-container">
+                  <Titles />
+                </div>
+
+                <div className="col-md-7 form-container">
+                  <Form
+                    getWeather={this.getWeather}
+                  />
+                  <Weather
+                    city       ={this.state.city}
+                    country    ={this.state.country}
+                    temperature={this.state.temperature}
+                    description={this.state.description}
+                    humidity   ={this.state.humidity}
+                    error      ={this.state.error}
+                  />
+                </div>
+
+              </div>
+            </div>
+        </div>
+    )
+  }
+}
+
+export default Current;
