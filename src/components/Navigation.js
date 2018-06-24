9b37d69f4c703e8597ec148b7166a50b453ffd1f
Karen <kjgamis@live.ca>
2018-06-23 23:45:06 -0400

    move navigation to new component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 const Navigation = () => (
   <div className='navbar'>
     <nav>
-      Navigation
+      <Link to='/'>Home</Link>
+      <Link to='/current'>Current Weather</Link>
+      <Link to='/ninehours'>3 Hour Intervals</Link>
     </nav>
   </div>
 )
