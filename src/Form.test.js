d0aef7e4152cb8942b24d1dc439806c7e8abec5d
Karen <kjgamis@live.ca>
2018-07-06 00:16:51 -0400

    add test files

---
+++ src/Form.test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Form from './components/Form';
---
+++ src/Weather.test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Weather from './components/Weather';
