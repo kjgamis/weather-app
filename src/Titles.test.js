35a1760a2fb50fd3eab11198d0175d32527e6c2e
Karen <kjgamis@live.ca>
2018-07-06 00:25:26 -0400

    add test to render titles in UI

--- src/Titles.test.js
+++ src/Titles.test.js
@@ -2,3 +2,15 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Titles from './components/Titles';
+
+it('renders title', () => {
+  const wrapper  = shallow(<Titles />)
+  const appTitle = <h1 className="app-title">Solar</h1>
+  expect(wrapper.contains(appTitle)).toEqual(true);
+});
+
+it('renders slogan', () => {
+  const wrapper  = shallow(<Titles />)
+  const appLine  = <p className='app-subtitle'>Weather Finder</p>
+  expect(wrapper.contains(appLine)).toEqual(true);
+});
