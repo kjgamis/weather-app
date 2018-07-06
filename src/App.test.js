06fdc3de217ba0ce7f472c83e8b4ede37e17716f
Karen <kjgamis@live.ca>
2018-07-05 23:34:33 -0400

    add simple tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+
+import Home from './components/Home'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders without crashing', () => {
+  shallow(<App />);
+});
+
+it('renders Home component', () => {
+  const wrapper = shallow(<Home />);
+  const name = <h1>Simple Weather App</h1>
+  expect(wrapper.contains(name)).toEqual(true);
+})
